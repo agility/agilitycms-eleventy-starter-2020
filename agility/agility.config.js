65fde87661fcdc47a8e59106502b36456b857e88
Joel Varty <joelvarty@outlook.com>
2020-11-10 07:28:25 -0500

    tweaked commands and updated post deets

--- agility/agility.config.js
+++ agility/agility.config.js
@@ -9,10 +9,11 @@ const agilityConfig = {
 	guid: process.env.AGILITY_GUID,
 	fetchAPIKey: process.env.AGILITY_API_FETCH_KEY, //Set your fetch apikey here
 	previewAPIKey: process.env.AGILITY_API_PREVIEW_KEY, //set your preview apikey
-	languageCode: process.env.AGILITY_LANGUAGE_CODE,  //the language for your website in Agility CMS
-	channelName: process.env.AGILITY_CHANNEL_NAME, //the name of your channel in Agility CMS
+	languageCode: (process.env.AGILITY_LANGUAGE_CODE || "en-us"),  //the language for your website in Agility CMS
+	channelName: (process.env.AGILITY_CHANNEL_NAME || "website"), //the name of your channel in Agility CMS
 	isPreviewMode: process.env.NODE_ENV === "development"
 }
+
 
 const getSyncClient = ({ isPreview }) => {
 
