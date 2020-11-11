452863a31e550b6e8db18cd29bd4b16d723169a7
Joel Varty <joelvarty@outlook.com>
2020-11-11 10:14:09 -0500

    added footer

--- pages/_data/global.js
+++ pages/_data/global.js
@@ -1,26 +1,44 @@
 const { getSyncClient, agilityConfig } = require('../../agility/agility.config')
 const truncate  = require('truncate-html')
+const { expandLinkedList } = require("../../agility/utils")
 
 async function getAgilityContent() {
 
 	const languageCode = agilityConfig.languageCode
 	const isPreview = agilityConfig.isPreviewMode
 	const channelName = agilityConfig.channelName
-
 	const syncClient = getSyncClient({isPreview})
-	let nestedSitemap = await syncClient.store.getSitemapNested({ channelName, languageCode })
+	const agility = syncClient.store
+
+
+	let nestedSitemap = await agility.getSitemapNested({ channelName, languageCode })
 
 	if (! nestedSitemap) return {}
 
-	let globalHeader = await syncClient.store.getContentList({ referenceName: "globalheader", languageCode })
+	let globalHeader = await agility.getContentList({ referenceName: "globalheader", languageCode })
 	if (globalHeader && globalHeader.length > 0) globalHeader = globalHeader[0]
 
-	let globalFooter = await syncClient.store.getContentList({ referenceName: "globalfooter", languageCode })
+	let globalFooter = await agility.getContentList({ referenceName: "globalfooter", languageCode })
 	if (globalFooter && globalFooter.length > 0) globalFooter = globalFooter[0]
 
+
+	//resolve the links...
+	globalFooter = await expandLinkedList({ agility, contentItem:globalFooter, languageCode,
+		fieldName: "column2Links",
+		sortIDField: "column2SortIDs"
+	})
+
+	globalFooter = await expandLinkedList({ agility, contentItem:globalFooter, languageCode,
+		fieldName: "column3Links",
+		sortIDField: "column3SortIDs"
+	})
+
+	globalFooter = await expandLinkedList({ agility, contentItem:globalFooter, languageCode,
+		fieldName: "column4Links",
+		sortIDField: "column4SortIDs"
+	})
+
 	let navigation = nestedSitemap.filter(n => n.visible.menu)
-
-
 
 	return {
 		navigation,
