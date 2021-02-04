93b429bd21b92c765e13d707160553583d2666d0
Joel Varty <joel@agilitycms.com>
2021-02-04 13:36:45 -0500

    tweaks

--- .eleventy.js
+++ .eleventy.js
@@ -13,22 +13,22 @@ const manifest = isDev
 	: JSON.parse(fs.readFileSync(manifestPath, { encoding: "utf8" }));
 
 
-async function imageShortcode(src, alt, sizes) {
-	let metadata = await Image(src, {
-		widths: [300, 600],
-		formats: ["avif", "jpeg"]
-	});
+	async function imageShortcode(src, alt, sizes) {
+		let metadata = await Image(src, {
+		  widths: [300, 600],
+		  formats: ["avif", "jpeg"]
+		});
 
-	let imageAttributes = {
-		alt,
-		sizes,
-		loading: "lazy",
-		decoding: "async",
-	};
+		let imageAttributes = {
+		  alt,
+		  sizes,
+		  loading: "lazy",
+		  decoding: "async",
+		};
 
-	// You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
-	return Image.generateHTML(metadata, imageAttributes);
-}
+		// You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
+		return Image.generateHTML(metadata, imageAttributes);
+	  }
 
 
 module.exports = function (eleventyConfig) {
